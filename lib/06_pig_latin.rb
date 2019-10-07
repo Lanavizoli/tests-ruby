4d4e32089fa9d7490c814114b0347e275784e7c5
lanavizoli <lana.vizoli.lvo@gmail.com>
2019-10-07 21:30:38 +0200

    Yo 2.0

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,12 +1,20 @@
-def translate (s)
-  alphabet = ["a".."z"].to_a
-  voyelle = ["aeiou"]
+def translate(phrase)
+  alphabet = ["a".."z"].to_a #.to_a il permet de tout prendre
+  voyelle = ["a", "e", "i", "o", "u", "y"]
   consonne = alphabet - voyelle
 
-if voyelle.include? s[0]
-  s + "ay"
+    if voyelle.include?(phrase[0]) #si la premiere lettre (0) de la phrase = voyelle -->
+            phrase + "ay" #mettre phrase + ay
 
+        elsif consonne.include?(phrase[0]) && voyelle.include?(phrase[2])
+            phrase[2..-1] + phrase[0..1] + "ay" #marche pour le 3&7
 
-
+        else consonne.include?(phrase[0]) && consonne.include?(phrase[2])
+            phrase[3..-1] + phrase[0..2] + "ay" #marche pour 5&6&8
+    end
 end
-end
+
+
+# else = .F.FFF.FF
+
+#elsif = FFF..F.F
